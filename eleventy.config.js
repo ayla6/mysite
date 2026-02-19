6720b194e08a6cbd3a0aeacddc0631825e7f5db9
ayla <ayla-git.barcode041@silomails.com>
2026-02-19 20:45:33 -0300

    boo
    Signed-off-by: ayla <ayla-git.barcode041@silomails.com>

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,49 +5,50 @@ import markdownit from "markdown-it";
 const md = markdownit();
 
 export default function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("favicon.png");
-  eleventyConfig.addPassthroughCopy(".well-known/*");
-  eleventyConfig.addPassthroughCopy("aataylactop.asc");
+	eleventyConfig.addPassthroughCopy("favicon.png");
+	eleventyConfig.addPassthroughCopy(".well-known/*");
+	eleventyConfig.addPassthroughCopy("_redirects");
+	eleventyConfig.addPassthroughCopy("aataylactop.asc");
 
-  eleventyConfig.addTemplateFormats("scss");
-  eleventyConfig.addExtension("scss", {
-    outputFileExtension: "css",
-    compile: async function (inputContent, inputPath) {
-      let result = sass.compileString(inputContent, {
-        loadPaths: [path.dirname(inputPath)],
-        style: "compressed",
-      });
-      return async () => {
-        return result.css;
-      };
-    },
-  });
+	eleventyConfig.addTemplateFormats("scss");
+	eleventyConfig.addExtension("scss", {
+		outputFileExtension: "css",
+		compile: async function (inputContent, inputPath) {
+			let result = sass.compileString(inputContent, {
+				loadPaths: [path.dirname(inputPath)],
+				style: "compressed",
+			});
+			return async () => {
+				return result.css;
+			};
+		},
+	});
 
-  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-    if (outputPath && outputPath.endsWith(".html")) {
-      return htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-        collapseInlineTagWhitespace: false,
-      });
-    }
-    return content;
-  });
+	eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+		if (outputPath && outputPath.endsWith(".html")) {
+			return htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true,
+				collapseInlineTagWhitespace: false,
+			});
+		}
+		return content;
+	});
 
-  eleventyConfig.addNunjucksFilter("markdown", function (value) {
-    return md.render(value);
-  });
+	eleventyConfig.addNunjucksFilter("markdown", function (value) {
+		return md.render(value);
+	});
 
-  eleventyConfig.addNunjucksFilter("format_date", function (value) {
-    return new Date(value).toISOString().slice(0, 16).replace("T", " ");
-  });
+	eleventyConfig.addNunjucksFilter("format_date", function (value) {
+		return new Date(value).toISOString().slice(0, 16).replace("T", " ");
+	});
 
-  eleventyConfig.addNunjucksFilter("truncate", function (value, length = 300) {
-    return value.slice(0, length);
-  });
+	eleventyConfig.addNunjucksFilter("truncate", function (value, length = 300) {
+		return value.slice(0, length);
+	});
 
-  eleventyConfig.addNunjucksFilter("getrkey", function (value) {
-    return value.slice(value.lastIndexOf("/") + 1);
-  });
+	eleventyConfig.addNunjucksFilter("getrkey", function (value) {
+		return value.slice(value.lastIndexOf("/") + 1);
+	});
 }
